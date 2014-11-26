d99ce8939753a61725e406967c3cf81059a0a6ec
tWido <tomas.wido@gmail.com>
2014-11-26 14:28:31 +0100

    Create soup.py

---
+++ soup.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import urllib2
+import re
+
+url = urllib2.urlopen("http://hladnystudent.zones.sk/")
+text = url.read()
+soup = BeautifulSoup(text)
+tables = soup.find_all('table')
+ 
+def upper(): 
+    hornasoup = BeautifulSoup(str(tables[1])) 
+
+    trs = hornasoup.find_all('tr')
+
+    for i in range (1,trs.__len__()):        
+         td = str(trs[i])
+         tdsoup = BeautifulSoup(td)
+         bordel = tdsoup.find_all('td')
+
+    match = re.findall(r'<td>(.*?)<span',str(bordel[1]),re.DOTALL)
+    bettermatch = re.findall(r'\n\s\s\s\s\s\s\s\s(.*?)\n',match[0])
+    return bettermatch
+
+def lower():
+    dolnasoup = BeautifulSoup(str(tables[2])) 
+    trs_dolna = dolnasoup.find_all('tr')
+
+    for i in range (1,trs_dolna.__len__()):        
+        dtd = str(trs_dolna[i])
+        dtdsoup = BeautifulSoup(dtd)
+        dbordel = dtdsoup.find_all('td')
+
+    dmatch = re.findall(r'<td>(.*?)<span',str(dbordel[1]),re.DOTALL)
+    dbettermatch = re.findall(r'\n\s\s\s\s\s\s\s\s(.*?)\n',dmatch[0])
+    return dbettermatch
