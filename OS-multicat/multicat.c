d0f17d612bf18e2a10d67ea64f2a6489feff81df
Wido <tomas.wido@gmail.com>
2015-03-25 19:54:35 +0100

    minor updates

--- OS-multicat/multicat.c
+++ OS-multicat/multicat.c
@@ -4,13 +4,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define SIZE 8*1024*1024
-//8 MB buffer
+#define SIZE 16*1024*1024
+//16 MB buffer
 
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond_read = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond_write = PTHREAD_COND_INITIALIZER;
+
 
 bool done = false;
 int reading_to = 1;
@@ -25,73 +26,89 @@ int buffer_2_dat = 0;
 void *read_to_buff(void *data){
   int readed;
     while(true){
-	if (done == true){break;}
 	pthread_mutex_lock(&lock);
 	switch (reading_to){
 	  case 1 :{
-	      if (buffer_1_dat > 0 ){
-		pthread_cond_wait(&cond_read, &lock);
+	      while (buffer_1_dat > 0 ) {
+		    pthread_cond_wait(&cond_read, &lock);
 		}
-	    readed = read(0, buffer_1 , SIZE);
-	    if (readed == 0){ done = true; }
-	    buffer_1_dat +=readed;
-	    reading_to = 2;
-	    pthread_cond_signal(&cond_write);
-	    break;
-	} 
+	      readed = read(0, buffer_1 , SIZE);
+	      buffer_1_dat +=readed;
+	      reading_to = 2;
+	      if (readed == 0){ 
+		done = true;
+		pthread_mutex_unlock(&lock);
+		pthread_cond_signal(&cond_write);
+		pthread_exit(NULL);
+	      }
+	      pthread_cond_signal(&cond_write);
+	      break;
+	  } 
 	  case 2 : {
-	      if (buffer_2_dat > 0 ){
-		pthread_cond_wait(&cond_read, &lock);
+	      while (buffer_2_dat > 0 ){
+		  pthread_cond_wait(&cond_read, &lock);
 		}
-	    readed = read(0,buffer_2,SIZE);
-	    if (readed == 0){ done = true;  }
-	    buffer_2_dat +=readed;
-	    reading_to = 1;
-	    pthread_cond_signal(&cond_write);
-	    break;
-	}	
-      }
+	      readed = read(0,buffer_2,SIZE);
+	      buffer_2_dat +=readed;
+	      reading_to = 1;
+	      if (readed == 0){ 
+		done = true;
+		pthread_mutex_unlock(&lock);
+		pthread_cond_signal(&cond_write);
+		pthread_exit(NULL);
+	      }
+	      pthread_cond_signal(&cond_write);
+	      break;
+	    }	
+	  }
       pthread_mutex_unlock(&lock);
+      if (readed== -1 ){
+		  printf("Error reading\n" );
+		}
     }
-    pthread_exit(NULL);
-    return NULL;
 }
 
 
 void *print_from_buff(void *data){
-    int wrote = 0;
+    int wrote;
     while(true){
 	pthread_mutex_lock(&lock);
 	switch (writing_from){
 	  case 1:{
-	    if ( buffer_1_dat == 0 ){
-		pthread_cond_wait(&cond_write, &lock);
+	      while ( buffer_1_dat == 0  && done  == false){ 
+		  pthread_cond_wait(&cond_write, &lock);
+		  }
+	      while (buffer_1_dat != 0){
+		  wrote = write(1, buffer_1, buffer_1_dat);  
+		  buffer_1_dat = buffer_1_dat -wrote;
+	      }
+	      writing_from = 2;
+	      pthread_cond_signal(&cond_read);
+	      break;
 	    }
-	    wrote = write(1, buffer_1, buffer_1_dat);  
-	    buffer_1_dat = buffer_1_dat -wrote;
-	    writing_from = 2;
-	    pthread_cond_signal(&cond_read);
-	    break;
-	}
 	
 	  case 2:{
-	    if ( buffer_2_dat == 0 ){
-		pthread_cond_wait(&cond_write, &lock);
+	      while( buffer_2_dat == 0  && done == false){
+		  pthread_cond_wait(&cond_write, &lock);
+	      }
+	      while (buffer_2_dat != 0){
+		  wrote = write(1, buffer_2, buffer_2_dat); 
+		  buffer_2_dat = buffer_2_dat - wrote;	
+	      }
+	      writing_from = 1;
+	      pthread_cond_signal(&cond_read);
+	      break;
 	    }
-	    wrote = write(1, buffer_2, buffer_2_dat); 
-	    buffer_2_dat = buffer_2_dat - wrote;
-	    writing_from = 1;
-	    pthread_cond_signal(&cond_read);
-	    break;
 	  }
-	}
+	if (done == true){ // (wrote == 0)  didn't work, don't have a clue why
+	    pthread_mutex_unlock(&lock);
+	    pthread_exit(NULL);
+	}  
 	pthread_mutex_unlock(&lock);
-        if (done == true){
-	    break;}
+	if (wrote == -1 ){
+	    printf("Error printing \n" );
+	    }
    }
-   
-    pthread_exit(NULL);
-    return NULL;
 }
 
 int main(int argc, char** argv) {
@@ -103,18 +120,22 @@ int main(int argc, char** argv) {
     if (create_read_stat != 0) {
 	printf("Error creating reading thread, error code: %d \n",create_read_stat );
     }
+    
     int create_printer_stat = pthread_create(&printer, NULL, print_from_buff,  NULL);
     if (create_printer_stat != 0) {
 	printf("Error creating printing thread, error code: %d \n",create_printer_stat );
     }
+    
     int end_status_reader = pthread_join(reader, NULL);
     if (end_status_reader != 0) {
 	printf("Reader error, error code: %d \n",end_status_reader );
     }
-   /* int end_status_printer = pthread_join(printer, NULL);
+    
+    int end_status_printer = pthread_join(printer, NULL);
     if (end_status_printer != 0) {
 	printf("Printer error, error code: %d \n",end_status_printer );
-   }*/
+   }
+   
    int mutex_status = pthread_mutex_destroy( &lock);
    if (mutex_status != 0) {
        printf("Mutex not destroyed, error code: %d \n",mutex_status );
